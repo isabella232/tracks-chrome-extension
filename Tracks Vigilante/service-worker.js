c0df540638709998f6086aff0050cd8de71e26f9
Daniel López <daniel.lopez@automattic.com>
2022-01-14 11:39:30 +0100

    Ability to pause/resume though context menu Ability to clear data from context menu Semicolon ESLint rule

--- Tracks Vigilante/service-worker.js
+++ Tracks Vigilante/service-worker.js
@@ -1,5 +1,5 @@
 ( function () {
-  'use strict'
+  'use strict';
 
   /**
    * The event data
@@ -19,61 +19,123 @@
   /** @type {TrackStorage} */
   const storage = {
     urlArray: [],
-  }
+  };
+
+  let vigilanteStatus = 'running';
+
+  /**
+   * Get Vigilante Status
+   */
+  async function getVigilanteStatus () {
+    return new Promise( ( resolve, reject ) => {
+      chrome.storage.local.get( 'vigilanteStatus', ( result ) => {
+        const error = chrome.runtime.lastError;
+        if ( error ) {
+          return reject( error );
+        }
+
+        Object.assign( vigilanteStatus, result );
+        chrome.contextMenus.update(
+          'vigilanteStatus',
+          {
+            checked: vigilanteStatus === 'running',
+          }
+        );
+        updateBadge( storage?.urlArray );
+        resolve();
+      } );
+    } );
+  };
+
+  chrome.contextMenus.removeAll();
+  chrome.contextMenus.create( {
+    title: 'Running',
+    id: 'vigilanteStatus',
+    type: 'checkbox',
+    checked: true,
+    contexts: [ 'action' ],
+  } );
+
+  chrome.contextMenus.create( {
+    title: 'Clear data',
+    id: 'clear',
+    checked: true,
+    contexts: [ 'action' ],
+  } );
+
+  chrome.contextMenus.onClicked.addListener(
+    ( e ) => {
+      if ( e.menuItemId === 'vigilanteStatus' ) {
+        if ( vigilanteStatus === 'running' ) {
+          vigilanteStatus = 'paused';
+        } else {
+          vigilanteStatus = 'running';
+        }
+        getVigilanteStatus();
+      }
+      if ( ( e.menuItemId === 'clear' ) ) {
+        clearData();
+      }
+    }
+  );
+
+  getVigilanteStatus();
 
   // See: https://developer.chrome.com/docs/extensions/reference/storage/#asynchronous-preload-from-storage
   chrome.action.onClicked.addListener( async () => {
     try {
-      await initStorage()
+      await initStorage();
     } catch ( error ) {
       // Ignore the error
     }
-  } )
+  } );
 
   chrome.webRequest.onCompleted.addListener( onCompletedListener,
-    { urls: [ '*://pixel.wp.com/*' ] }, [ 'responseHeaders' ] )
+    { urls: [ '*://pixel.wp.com/*' ] }, [ 'responseHeaders' ] );
 
   chrome.runtime.onMessage.addListener(
     function ( request ) {
       if ( request.msg === 'Clear' ) {
-        clearData()
+        clearData();
       }
     }
-  )
+  );
 
   function hasBeenReplaced ( url ) {
-    return url.includes( 'https://pixel.wp.com' ) || url.includes( 'http://pixel.wp.com' )
+    return url.includes( 'https://pixel.wp.com' ) || url.includes( 'http://pixel.wp.com' );
   }
 
   /**
-   *
    * @param { Event } details for the completed request.
    */
   function onCompletedListener ( details ) {
-    let url = details.url.replace( 'https://pixel.wp.com/t.gif?', '' ).replace( 'http://pixel.wp.com/t.gif?', '' )
-    let type = ''
+    if ( vigilanteStatus !== 'running' ) {
+      return;
+    }
+    let url = details.url.replace( 'https://pixel.wp.com/t.gif?', '' ).replace( 'http://pixel.wp.com/t.gif?', '' );
+    let type = '';
 
     // TODO A bit ugly filter, to be improved for better readability
     if ( hasBeenReplaced( url ) ) {
-      url = details.url.replace( 'https://pixel.wp.com/g.gif?', '' ).replace( 'http://pixel.wp.com/g.gif?', '' )
+      url = details.url.replace( 'https://pixel.wp.com/g.gif?', '' ).replace( 'http://pixel.wp.com/g.gif?', '' );
       if ( hasBeenReplaced( url ) ) {
-        url = details.url.replace( 'https://pixel.wp.com/boom.gif?', '' ).replace( 'http://pixel.wp.com/boom.gif?', '' )
+        url = details.url.replace( 'https://pixel.wp.com/boom.gif?', '' ).replace( 'http://pixel.wp.com/boom.gif?', '' );
         if ( hasBeenReplaced( url ) ) {
-          console.log( `Unknown pixel ${ url }` )
-          return
+          console.log( `Unknown pixel ${ url }` );
+          return;
         } else {
-          type = 'grafana'
+          type = 'grafana';
         }
       } else {
-        type = 'external'
+        type = 'external';
       }
     } else {
-      type = 'tracks-event'
+      type = 'tracks-event';
     }
-    const urlSearchParams = new URLSearchParams( url )
-    const params = Object.fromEntries( urlSearchParams.entries() )
+    const urlSearchParams = new URLSearchParams( url );
+    const params = Object.fromEntries( urlSearchParams.entries() );
 
-    storeData( params, type )
+    storeData( params, type );
   }
 
   /**
@@ -87,7 +149,7 @@
       data: {
         queryParams: params,
       },
-    } )
+    } );
   }
 
   /**
@@ -96,24 +158,24 @@
    * @param string type (tracks-event, external, grafana)
    */
   async function storeData ( params, type ) {
-    const currentDate = new Date()
-    const stringDate = currentDate.toLocaleTimeString()
-    const urlArray = storage?.urlArray?.slice() || []
+    const currentDate = new Date();
+    const stringDate = currentDate.toLocaleTimeString();
+    const urlArray = storage?.urlArray?.slice() || [];
 
-    urlArray.push( { key: params._en, values: params, time: stringDate, type: type } )
+    urlArray.push( { key: params._en, values: params, time: stringDate, type: type } );
 
-    await updateStorage( { urlArray } )
-    updateBadge( urlArray )
-    sendMessageToVigilante( params )
+    await updateStorage( { urlArray } );
+    updateBadge( urlArray );
+    sendMessageToVigilante( params );
   }
 
   /**
    * Clear the data
    */
   async function clearData () {
-    await updateStorage( { urlArray: [] } )
-    updateBadge( [] )
-    sendMessageToVigilante( [] )
+    await updateStorage( { urlArray: [] } );
+    updateBadge( [] );
+    sendMessageToVigilante( [] );
   }
 
   /**
@@ -122,7 +184,11 @@
    * @param {TrackEvent[]} trackEvents our data array
    */
   function updateBadge ( trackEvents ) {
-    chrome.action.setBadgeText( { text: trackEvents.length > 0 ? trackEvents.length.toString() : '' } )
+    if ( vigilanteStatus === 'running' ) {
+      chrome.action.setBadgeText( { text: trackEvents.length > 0 ? trackEvents.length.toString() : '' } );
+    } else {
+      chrome.action.setBadgeText( { text: 'STOP' } );
+    }
   }
 
   /**
@@ -131,15 +197,15 @@
   async function initStorage () {
     return new Promise( ( resolve, reject ) => {
       chrome.storage.local.get( 'urlArray', ( result ) => {
-        const error = chrome.runtime.lastError
+        const error = chrome.runtime.lastError;
         if ( error ) {
-          return reject( error )
+          return reject( error );
         }
 
-        Object.assign( storage, result )
-        resolve()
-      } )
-    } )
+        Object.assign( storage, result );
+        resolve();
+      } );
+    } );
   }
 
   /**
@@ -148,49 +214,49 @@
    */
   async function updateStorage ( nextStorage ) {
     return new Promise( ( resolve ) => {
-      Object.assign( storage, nextStorage )
-      chrome.storage.local.set( nextStorage, () => resolve() )
-    } )
+      Object.assign( storage, nextStorage );
+      chrome.storage.local.set( nextStorage, () => resolve() );
+    } );
   }
 
   // Following code is to "revive" plugin if it has dead because of inactivity
-  let lifeline
+  let lifeline;
 
-  keepAlive()
+  keepAlive();
 
   chrome.runtime.onConnect.addListener( port => {
     if ( port.name === 'keepAlive' ) {
-      lifeline = port
-      setTimeout( keepAliveForced, 295e3 ) // 5 minutes minus 5 seconds
-      port.onDisconnect.addListener( keepAliveForced )
+      lifeline = port;
+      setTimeout( keepAliveForced, 295e3 ); // 5 minutes minus 5 seconds
+      port.onDisconnect.addListener( keepAliveForced );
     }
-  } )
+  } );
 
   function keepAliveForced () {
-    lifeline?.disconnect()
-    lifeline = null
-    keepAlive()
+    lifeline?.disconnect();
+    lifeline = null;
+    keepAlive();
   }
 
   async function keepAlive () {
-    if ( lifeline ) return
+    if ( lifeline ) return;
     for ( const tab of await chrome.tabs.query( { url: '*://*/*' } ) ) {
       try {
         await chrome.scripting.executeScript( {
           target: { tabId: tab.id },
           function: () => chrome.runtime.connect( { name: 'keepAlive' } ),
           // `function` will become `func` in Chrome 93+
-        } )
-        chrome.tabs.onUpdated.removeListener( retryOnTabUpdate )
-        return
+        } );
+        chrome.tabs.onUpdated.removeListener( retryOnTabUpdate );
+        return;
       } catch ( e ) { }
     }
-    chrome.tabs.onUpdated.addListener( retryOnTabUpdate )
+    chrome.tabs.onUpdated.addListener( retryOnTabUpdate );
   }
 
   async function retryOnTabUpdate ( tabId, info, tab ) {
     if ( info.url && ( /^(file|https?):/.test( info.url ) ) ) {
-      keepAlive()
+      keepAlive();
     }
   }
-} )()
+} )();
